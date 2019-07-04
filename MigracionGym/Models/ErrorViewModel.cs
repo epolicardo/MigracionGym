ed41b3639979b56d1cdb72d259e1217c3bbc56c5
Emiliano Policardo <emilianopolicardo@gmail.com>
2019-07-04 20:39:12 -0300

    Agregar archivos de proyecto.

---
+++ MigracionGym/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MigracionGym.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
