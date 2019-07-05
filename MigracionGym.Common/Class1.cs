f243af83e2c9cde28b1dc2e119dfe0a78b7982f1
Emiliano Policardo <emilianopolicardo@gmail.com>
2019-07-04 21:20:56 -0300

    Estructura inicial

---
+++ MigracionGym.Common/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace MigracionGym.Common
+{
+    public class Class1
+    {
+    }
+}
---
+++ MigracionGym.UI.Classic/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace MigracionGym.UI.Classic
+{
+    public class Class1
+    {
+    }
+}
